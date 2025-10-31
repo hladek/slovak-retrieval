6ead71523d287bf441a987eca8df966798a7da0b
Daniel Hladek <daniel.hladek@tuke.sk>
2025-10-31 16:34:40 +0100

    z

---
+++ data/get_webfaq.py
@@ -0,0 +1,8 @@
+# https://towardsdatascience.com/how-to-train-a-bert-model-from-scratch-72cfce554fc6
+from datasets import load_dataset
+import sys
+
+
+dataset = load_dataset("PaDaS-Lab/webfaq" , name="slk", split="default", streaming=True)
+for sample in dataset:
+    print(sample)
