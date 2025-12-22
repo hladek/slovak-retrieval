1e20935e75dbc45d4d591a33f57757ca7c13147b
Daniel Hladek <daniel.hladek@tuke.sk>
2025-12-22 17:56:13 +0000

    zz

--- data/get_mqa.py
+++ data/get_mqa.py
@@ -2,21 +2,34 @@
 from datasets import load_dataset
 import sys
 import json
-
+import collections
 
 dataset = load_dataset("clips/mqa" , name="sk-all-question", split="train", streaming=True)
 # id, text, name, domain, answers
+prev_domain = None
+questions = []
+answers = collections.deque()
 for sample in dataset:
-    answers = sample["answers"]
-    text = sample["text"]
+    sample_answers = sample["answers"]
+    question = sample["name"]
     did = sample["id"]
-    if len(answers) == 0:
-        continue
-    for answer in answers:
-        doc = {
-                "id":did,
-                "question": text,
-                "answer": answer,
-        }
-        print(json.dumps(doc))
-    print(sample)
+    domain = sample["domain"]
+    answer = sample_answers[0]["text"]
+    if prev_domain is not None and domain != prev_domain:
+        rot = 2
+        good_answers = list(answers)
+        answers.rotate(1)
+        bad_answers = list(answers)
+        for question,answer,bad_answer in zip(questions,good_answers,bad_answers):
+            print(question)
+            print("---")
+            print(answer)
+            print("---")
+            print(bad_answer)
+            print(">>>")
+        questions = []
+        answers.clear()
+
+    prev_domain = domain
+    questions.append(question)
+    answers.append(answer)
